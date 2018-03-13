18b8e9e2ee5552baf4386392e99c469963baf022
bobowang2017 <409879654@qq.com>
2018-03-13 17:16:30 +0800

    提交代码

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -8,8 +8,7 @@ from rest_framework import serializers
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username']
-        # read_only_fields = ['id']
+        fields = ['id', 'nick_name', 'gender', 'head_img_url', 'city', 'country', 'province', 'create_time', 'last_login']
 
     def create(self, validated_data):
         user, created = User.objects.get_or_create(id=validated_data['id'])
