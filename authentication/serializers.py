9f88b71103c7e3198e97b77a5944c959b7271f75
bobowang2017 <409879654@qq.com>
2018-03-13 17:12:51 +0800

    提交代码

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -1,4 +1,6 @@
 # coding: utf-8
+import datetime
+
 from authentication.models import User
 from rest_framework import serializers
 
@@ -8,4 +10,9 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ['id', 'username']
         # read_only_fields = ['id']
+
+    def create(self, validated_data):
+        user, created = User.objects.get_or_create(id=validated_data['id'])
+        user.last_login = datetime.datetime.now()
+        user.save()
 
