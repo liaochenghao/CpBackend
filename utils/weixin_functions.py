17533b1d3fb6f212fc5b060d17a4465005e97f6b
bobowang2017 <409879654@qq.com>
2018-03-23 18:17:04 +0800

    提交代码

--- utils/weixin_functions.py
+++ utils/weixin_functions.py
@@ -6,6 +6,7 @@ import requests
 from rest_framework import exceptions
 from CpBackend.settings import WX_SMART_CONFIG
 from authentication.models import User
+from register.models import NewCornRecord
 from ticket.functions import TicketAuthorize
 
 logger = logging.getLogger('django')
@@ -31,10 +32,17 @@ class WxInterface:
         res = response.json()
         if res.get('openid') and res.get('session_key'):
             logger.info(res)
-            user, created = User.objects.get_or_create(open_id=res['openid'])
-            user.last_login = datetime.datetime.now()
-            user.session_key = res['session_key']
-            user.save()
+            # 首先查询数据库中是否存在该用户信息
+            user = User.objects.filter(open_id=res['openid']).first()
+            if not user:
+                # 如果用户不存在，则向数据库插入数据
+                user = User.objects.create(open_id=res['openid'], last_login=datetime.datetime.now(),
+                                           session_key=res['session_key'])
+                # 同时给用户分配new corn 20
+                NewCornRecord.objects.create(user=user, operation=2, corn=20, balance=20)
+            else:
+                user.last_login = datetime.datetime.now()
+                user.save()
             ticket = TicketAuthorize.create_ticket(res['openid'])
             return {'user_id': user.open_id, 'ticket': ticket}
         else:
