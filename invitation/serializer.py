5adf13bb23087f335b341478a8412db8f3e41000
bobowang2017 <409879654@qq.com>
2018-03-28 14:45:54 +0800

    提交代码

--- invitation/serializer.py
+++ invitation/serializer.py
@@ -1,5 +1,4 @@
 # coding: utf-8
-import uuid
 from rest_framework import serializers
 from invitation.models import Invitation
 
@@ -7,5 +6,10 @@ from invitation.models import Invitation
 class InvitationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Invitation
-        fields = ['id', 'inviter', 'invitee', 'create_time', 'status', 'update_at']
-        read_only_fields = ['id', 'inviter']
+        fields = ['id', 'inviter', 'invitee', 'create_time', 'status', 'update_at', 'expire_at']
+        read_only_fields = ['id', 'inviter', 'expire_at']
+
+    def create(self, validated_data):
+        print('3333333333333336666666666666')
+        validated_data['expire_at'] = validated_data['create_time']
+        print(type(validated_data['expire_at']))
