6a540132df6d082e5d82c13d6ee3e94eafbede43
bobowang2017 <409879654@qq.com>
2018-03-31 13:44:23 +0800

    提交代码

--- activity/views.py
+++ activity/views.py
@@ -3,7 +3,6 @@ from rest_framework.views import APIView
 from activity.models import Activity
 from activity.serializers import ActivitySerializer
 from rest_framework import exceptions
-
 from register.models import Register
 
 
@@ -11,12 +10,13 @@ class ActivityView(APIView):
     def get(self, request):
         params = request.query_params
         if not params.get('name'):
-            raise exceptions.ValidationError('Param activity name is None')
-        activity = Activity.objects.filter(name=params.get('name')).first()
+            raise exceptions.ValidationError('参数name(活动名称)不能为空')
+        activity = Activity.objects.filter(name=params.get('name'))
         if not activity:
-            raise exceptions.ValidationError('Can not find activity information')
-        serializer = ActivitySerializer(activity)
+            raise exceptions.ValidationError('未查询到当前活动信息')
+        serializer = ActivitySerializer(activity[0])
         result = serializer.data
+        # 查询报名表中报名人数
         user_count = Register.objects.filter(activity=1).count()
         result['user_count'] = user_count
         return Response(result)
