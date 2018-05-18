c48a5ac109f80ed8bcd3ae1702df8e066158230e
bobowang2017 <409879654@qq.com>
2018-05-18 10:49:12 +0800

    提交代码

--- user_info/function.py
+++ user_info/function.py
@@ -1,1 +1,14 @@
 # coding: utf-8
+def get_constellation(month, day):
+    """
+    根据生日计算星座
+    :param month: 月份
+    :param day: 天数
+    :return: 
+    """
+    dates = (21, 20, 21, 21, 22, 22, 23, 24, 24, 24, 23, 22)
+    constellations = ("摩羯", "水瓶", "双鱼", "白羊", "金牛", "双子", "巨蟹", "狮子", "处女", "天秤", "天蝎", "射手", "摩羯")
+    if day < dates[month - 1]:
+        return constellations[month - 1]
+    else:
+        return constellations[month]
